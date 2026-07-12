df4149df937ebb8edaa1f17109389374a2fe3877
Dragonhatch1 <Dragonhatch1@hotmail.com>
2026-07-11 20:57:30 -0500

    tryin stuff

--- src/main/java/com/xyrth/shrunken/event/PermanentPotionHandler.java
+++ src/main/java/com/xyrth/shrunken/event/PermanentPotionHandler.java
@@ -1,16 +1,27 @@
 package com.xyrth.shrunken.event;
 
+import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import net.minecraftforge.client.event.RenderLivingEvent.Pre;
+import net.minecraftforge.client.event.RenderLivingEvent.Post;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
 
 public class PermanentPotionHandler {
 
+    private static Method methodEntitySetSize;
+
+
     @SubscribeEvent
-    public void onLivingUpdate(LivingUpdateEvent event) {
+    public void onLivingUpdate(LivingUpdateEvent event) throws InvocationTargetException, IllegalAccessException {
         if (!(event.entity instanceof EntityPlayer)) return;
 
         EntityPlayer player = (EntityPlayer) event.entity;
@@ -18,16 +29,29 @@ public class PermanentPotionHandler {
         // Only run server-side to avoid double application
         if (player.worldObj.isRemote) return;
 
-        // Reapply when effect is missing or about to expire (< 10 ticks)
-        PotionEffect current = player.getActivePotionEffect(Potion.potionTypes[55]);
-        if (current == null || current.getDuration() < 10) {
-            player.addPotionEffect(
-                new PotionEffect(
-                    55, // Effect ID
-                    72000, // Duration in ticks (10 seconds)
-                    0, // Amplifier (0 = level I)
-                    false // isAmbient (false = show particles)
-                ));
-        }
+        player.eyeHeight = 0.03F;
+            player.getEyeHeight();
+
+        float width = 0.50F;
+        float height = 0.50F;
+
+        methodEntitySetSize = ReflectionHelper.findMethod(Entity.class, event.entity, new String[]{"setSize", "func_70105_a", "a"}, new Class[]{Float.TYPE, Float.TYPE});
+        System.out.println(methodEntitySetSize);
+        methodEntitySetSize.invoke(event.entity, new Object[]{width, height});
+        System.out.println("Current Height:" + event.entity.height);
+
+
+    }
+    @SubscribeEvent
+    public void onLivingRender(Pre event) {
+        GL11.glPushMatrix();
+        GL11.glTranslated(event.x, event.y, event.z);
+        float scale = 0.25F;
+        GL11.glScalef(scale, scale, scale);
+        GL11.glTranslated(-event.x, -event.y, -event.z);
+    }
+
+    public void onLivingRender(World world, EntityLivingBase entity, Post event) {
+        GL11.glPopMatrix();
     }
 }
