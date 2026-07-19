58d9e0babe07ce1b53e9b9282ce5426a9bd358ea
Dragonhatch1 <Dragonhatch1@hotmail.com>
2026-07-19 18:11:54 -0500

    playing around

--- src/main/java/com/xyrth/shrunken/client/PlayerSizeHandler.java
+++ src/main/java/com/xyrth/shrunken/client/PlayerSizeHandler.java
@@ -3,11 +3,13 @@ package com.xyrth.shrunken.client;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.relauncher.ReflectionHelper;
+import javafx.scene.Camera;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIPlay;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderLivingEvent.Pre;
 import net.minecraftforge.client.event.RenderLivingEvent.Post;
 import org.lwjgl.opengl.GL11;
@@ -20,10 +22,12 @@ import java.util.Arrays;
 public class PlayerSizeHandler {
 
     private static Method methodSetPlayerSize;
+    private static float defaultWidth = 0.6F;
+    private static float defaultHeight= 1.8F;
+    private static float scale = 0.25F; // 1.0 normal size | 1.5 50% bigger
 
     @SubscribeEvent
     public void onLivingRender(Pre event){
-        float scale = 0.5F; // 1.0 normal size | 1.5 50% bigger
         GL11.glTranslated(event.x, event.y, event.z);
         GL11.glPushMatrix();
         GL11.glScalef(scale, scale, scale);
@@ -42,7 +46,9 @@ public class PlayerSizeHandler {
 
         methodSetPlayerSize = ReflectionHelper.findMethod(Entity.class, player, new String[]{"setSize", "func_70105_a", "a"}, new Class[]{Float.TYPE, Float.TYPE});
 
-        //TODO need to change by scale
-        methodSetPlayerSize.invoke(player, 0.3F, 0.9F);
+        methodSetPlayerSize.invoke(player, (defaultWidth * scale), (defaultHeight * scale));
+        player.eyeHeight = defaultHeight * scale;
     }
+
+
 }
