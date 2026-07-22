0501809c2317164e12b2ea51b93443da988b2c1f
Dragonhatch1 <Dragonhatch1@hotmail.com>
2026-07-21 21:26:11 -0500

    Updated and testing on main. Currently Working.

--- src/main/java/com/xyrth/shrunken/client/PlayerSizeHandler.java
+++ src/main/java/com/xyrth/shrunken/client/PlayerSizeHandler.java
@@ -1,6 +1,5 @@
 package com.xyrth.shrunken.client;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
--- src/main/java/com/xyrth/shrunken/mixin/MixinMinecraft.java
+++ src/main/java/com/xyrth/shrunken/mixin/MixinMinecraft.java
@@ -1,19 +1,24 @@
 package com.xyrth.shrunken.mixin;
 
-import com.xyrth.shrunken.client.ShrunkenState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
-import net.minecraft.entity.Entity;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import com.xyrth.shrunken.client.ShrunkenState;
+
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
 
-    @Redirect(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/EntityClientPlayerMP;isEntityInsideOpaqueBlock()Z"))
-    private boolean shrunken$suppressForcedFirstPerson(EntityClientPlayerMP player){
-        if (ShrunkenState.getScale() < 1.0F ) {
+    @Redirect(
+        method = "runGameLoop",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/entity/EntityClientPlayerMP;isEntityInsideOpaqueBlock()Z"))
+    private boolean shrunken$suppressForcedFirstPerson(EntityClientPlayerMP player) {
+        if (ShrunkenState.getScale() < 1.0F) {
             return false;
         }
         return player.isEntityInsideOpaqueBlock();
